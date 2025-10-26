9e37f52d78d0db52d5ed14c15e5ee7a250438274
Anar Farzaliyev <a.farzaliyev@tayqatech.com>
2025-10-27 00:05:11 +0400

    ApplicationDbContext added.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+
+        public DbSet<Customer> Products => Set<Customer>();
+
+        public DbSet<Customer> Orders => Set<Customer>();
+
+        public DbSet<Customer> OrderItems => Set<Customer>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
